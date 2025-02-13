21662ddb32646f7bd3c7b6a19197940a75110bc3
zee-ham-su <hsufiian@yahoo.com>
2025-02-13 06:57:25 +0000

    fix: update CI workflow to trigger on pull requests to main branch

--- api/routes/books.py
+++ api/routes/books.py
@@ -32,12 +32,14 @@ db.books = {
     ),
 }
 
+
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def get_book(book_id: int) -> Book:
     book = db.books.get(book_id)
     if not book:
         raise HTTPException(status_code=404, detail="Book not found")
     return book
+
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(book: Book):
@@ -46,11 +48,13 @@ async def create_book(book: Book):
         status_code=status.HTTP_201_CREATED, content=book.model_dump()
     )
 
+
 @router.get(
     "/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK
 )
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
+
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
@@ -58,6 +62,7 @@ async def update_book(book_id: int, book: Book) -> Book:
         status_code=status.HTTP_200_OK,
         content=db.update_book(book_id, book).model_dump(),
     )
+
 
 @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int) -> None:
